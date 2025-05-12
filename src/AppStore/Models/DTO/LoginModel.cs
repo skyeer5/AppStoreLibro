ddb4818dacdbb6fe421fbb8840f69ba84fd6f910
skyeer <danielpineda@gmail.com>
2025-05-12 14:33:14 -0600

    Arreglando el LogginModel con su Required

--- src/AppStore/Models/DTO/LoginModel.cs
+++ src/AppStore/Models/DTO/LoginModel.cs
@@ -1,9 +1,13 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace AppStore.Models.DTO
 {
     public class LoginModel
     {
+        [Required]
         public string? username {get;set;}
+        [Required]
         public string? password {get;set;}
     }
 }
